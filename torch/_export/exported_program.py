4bf6527f92d8182d7628397c9c14bfbacf715d1e
angelayi <yiangela7@gmail.com>
2023-11-10 16:04:11 -0800

    [export] Move lifted tensors out of state_dict
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/112413
    
    https://github.com/pytorch/pytorch/issues/110484
    
    
    
    cc @voznesenskym @penguinwu @EikanWang @jgong5 @Guobing-Chen @XiaobingSuper @zhuhaozhe @blzheng @wenzhe-nrv @jiayisunx @peterbell10 @ipiszy @yf225 @chenyang78 @kadeng @muchulee8 @aakhundov @ColinPeppler @avikchaudhuri @gmagogsfm @zhxchen17 @tugsbayasgalan
    
    @imported-using-ghimport
    
    Differential Revision: [D50933212](https://our.internmc.facebook.com/intern/diff/D50933212/)
    ghstack-source-id: 207223550

--- torch/_export/exported_program.py
+++ torch/_export/exported_program.py
@@ -50,7 +50,7 @@ class CallSpec:
     out_spec: Optional[pytree.TreeSpec]
 
 
-def _unlift(gm, inp_pos_to_param_buffer_name, in_spec, out_spec, state_dict, buffers_to_mutate, user_outputs):
+def _unlift(gm, inp_pos_to_param_buffer_name, in_spec, out_spec, state_dict, tensor_constants, buffers_to_mutate):
     count = 0
     buffer_name_to_node = {}
     # Step 1: make lifted params as get_attr
@@ -135,8 +135,14 @@ def _unlift(gm, inp_pos_to_param_buffer_name, in_spec, out_spec, state_dict, buf
             for ix, operand in enumerate(operands):
                 if operand.target in inp_pos_to_param_buffer_name.values():
                     inp_pos_to_param_buffer_name_for_submod[ix] = operand.target
-                    true_gm.register_buffer(operand.target, state_dict[operand.target])
-                    false_gm.register_buffer(operand.target, state_dict[operand.target])
+                    if operand.target in state_dict:
+                        value = state_dict[operand.target]
+                    elif operand.target in tensor_constants:
+                        value = tensor_constants[operand.target]
+                    else:
+                        raise RuntimeError("Unable to find value for ", operand.target)
+                    true_gm.register_buffer(operand.target, value)
+                    false_gm.register_buffer(operand.target, value)
                 else:
                     real_operands.append(operand)
             node.args = (pred, true_graph, false_graph, real_operands)
@@ -149,8 +155,8 @@ def _unlift(gm, inp_pos_to_param_buffer_name, in_spec, out_spec, state_dict, buf
                 in_spec,
                 None,
                 state_dict,
+                tensor_constants,
                 buffers_to_mutate,
-                user_outputs,
             )
             _unlift(
                 false_gm,
@@ -158,8 +164,8 @@ def _unlift(gm, inp_pos_to_param_buffer_name, in_spec, out_spec, state_dict, buf
                 in_spec,
                 None,
                 state_dict,
+                tensor_constants,
                 buffers_to_mutate,
-                user_outputs,
             )
         if node.op == "call_function" and node.target.__name__ == "map_impl":
             body_graph, num_mapped, *operands = node.args
@@ -174,7 +180,13 @@ def _unlift(gm, inp_pos_to_param_buffer_name, in_spec, out_spec, state_dict, buf
             for ix, operand in enumerate(operands):
                 if operand.target in inp_pos_to_param_buffer_name.values():
                     inp_pos_to_buffer_name_for_submod[ix] = operand.target
-                    body_gm.register_buffer(operand.target, state_dict_for_lookup[operand.target])
+                    if operand.target in state_dict_for_lookup:
+                        value = state_dict_for_lookup[operand.target]
+                    elif operand.target in tensor_constants:
+                        value = tensor_constants[operand.target]
+                    else:
+                        raise RuntimeError(f"Unable to find value for {operand.target}")
+                    body_gm.register_buffer(operand.target, value)
                 else:
                     real_operands.append(operand)
             node.args = (body_graph, num_mapped, *real_operands)
@@ -187,15 +199,15 @@ def _unlift(gm, inp_pos_to_param_buffer_name, in_spec, out_spec, state_dict, buf
                 in_spec,
                 None,
                 state_dict,
+                tensor_constants,
                 buffers_to_mutate,
-                user_outputs,
             )
     gm.graph.lint()
     gm.graph.eliminate_dead_code()
     gm.recompile()
     return gm
 
-def _construct_inp_pos_to_param_buffer_name(new_gm, graph_signature, state_dict):
+def _construct_inp_pos_to_param_buffer_name(new_gm, graph_signature, state_dict, tensor_constants=None):
     # TODO Fix the period in params/buffers names later
     # maybe a pass to replace graph signature with fixed names
     param_buffer_name_to_corrected_name = {}
@@ -213,6 +225,13 @@ def _construct_inp_pos_to_param_buffer_name(new_gm, graph_signature, state_dict)
                 param_buffer_name_to_corrected_name[name] = name.replace(".", "_")
             else:
                 new_gm.register_parameter(name, value)
+
+    if tensor_constants is not None and len(tensor_constants) > 0:
+        assert hasattr(graph_signature, "lifted_tensor_constants")
+        for name, value in tensor_constants.items():
+            if name in graph_signature.lifted_tensor_constants:
+                new_gm.register_buffer(name, value)
+                param_buffer_name_to_corrected_name[name] = name
 
     count = 0
     inp_pos_to_param_buffer_name = {}
@@ -234,6 +253,11 @@ def _construct_inp_pos_to_param_buffer_name(new_gm, graph_signature, state_dict)
                     ] = param_buffer_name_to_corrected_name[param_name]
                 else:
                     inp_pos_to_param_buffer_name[count] = param_name
+            if hasattr(graph_signature, "inputs_to_lifted_tensor_constants"):
+                if node.name in graph_signature.inputs_to_lifted_tensor_constants:
+                    inp_pos_to_param_buffer_name[
+                        count
+                    ] = graph_signature.inputs_to_lifted_tensor_constants[node.name]
             count += 1
 
     return inp_pos_to_param_buffer_name
@@ -241,7 +265,7 @@ def _construct_inp_pos_to_param_buffer_name(new_gm, graph_signature, state_dict)
 def unlift_exported_program_lifted_states(ep: torch.export.ExportedProgram) -> torch.nn.Module:
     new_gm = copy.deepcopy(ep.graph_module)
     inp_pos_to_param_buffer_name = _construct_inp_pos_to_param_buffer_name(
-        new_gm, ep.graph_signature, ep.state_dict
+        new_gm, ep.graph_signature, ep.state_dict, ep.tensor_constants
     )
     new_gm = _unlift(
         new_gm,
@@ -249,8 +273,8 @@ def unlift_exported_program_lifted_states(ep: torch.export.ExportedProgram) -> t
         ep.call_spec.in_spec,
         ep.call_spec.out_spec,
         ep.state_dict,
+        ep.tensor_constants,
         ep.graph_signature.buffers_to_mutate,
-        ep.graph_signature.user_outputs,
     )
     new_gm.meta.update(ep.graph_module.meta)
     return new_gm
